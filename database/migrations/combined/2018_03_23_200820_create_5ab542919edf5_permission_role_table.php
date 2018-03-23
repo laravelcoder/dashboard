e847dcce21fb18a5c9275c087fe448ecee113b89
QUICKADMINPANEL <info@laraveldaily.com>
2018-03-23 18:20:55 +0000

    QuickAdminPanel Deployment ID:1091 on 2018-03-23 20:20:55

---
+++ database/migrations/combined/2018_03_23_200820_create_5ab542919edf5_permission_role_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Create5ab542919edf5PermissionRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(! Schema::hasTable('permission_role')) {
+            Schema::create('permission_role', function (Blueprint $table) {
+                $table->integer('permission_id')->unsigned()->nullable();
+                $table->foreign('permission_id', 'fk_p_134990_134991_role_p_5ab542919ef3a')->references('id')->on('permissions')->onDelete('cascade');
+                $table->integer('role_id')->unsigned()->nullable();
+                $table->foreign('role_id', 'fk_p_134991_134990_permis_5ab542919efc9')->references('id')->on('roles')->onDelete('cascade');
+                
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_role');
+    }
+}
---
+++ database/seeds/RoleSeedPivot.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleSeedPivot extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+            
+            1 => [
+                'permission' => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+            ],
+            3 => [
+                'permission' => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+            ],
+            4 => [
+                'permission' => [1, 2, 3, 4, 6, 9, 11, 14, 16, 17, 18, 19],
+            ],
+
+        ];
+
+        foreach ($items as $id => $item) {
+            $role = \App\Role::find($id);
+
+            foreach ($item as $key => $ids) {
+                $role->{$key}()->sync($ids);
+            }
+        }
+    }
+}
