3a93f69621299741eeb822bdb592b017eca5cbe3
laravelcoder <pmadsen2013@gamil.comgit>
2018-04-06 21:25:36 -0600

    updated migration fixes and dev route to web.php

--- database/migrations/2018_03_28_204051_create_5abbd3a07cb2e_contact_contact_table.php
+++ database/migrations/2018_03_28_204051_create_5abbd3a07cb2e_contact_contact_table.php
@@ -12,15 +12,7 @@ class Create5abbd3a07cb2eContactContactTable extends Migration
      */
     public function up()
     {
-        if(! Schema::hasTable('contact_contact')) {
-            Schema::create('contact_contact', function (Blueprint $table) {
-                $table->integer('contact_id')->unsigned()->nullable();
-                $table->foreign('contact_id', 'fk_p_135003_135003_contac_5abbd3a07ccd7')->references('id')->on('contacts')->onDelete('cascade');
-                $table->integer('contact_id')->unsigned()->nullable();
-                $table->foreign('contact_id', 'fk_p_135003_135003_contac_5abbd3a07cda8')->references('id')->on('contacts')->onDelete('cascade');
-                
-            });
-        }
+
     }
 
     /**
--- database/migrations/combined/2018_04_07_033437_create_5ac8121a8ecb2_contact_contact_table.php
+++ database/migrations/combined/2018_04_07_033437_create_5ac8121a8ecb2_contact_contact_table.php
@@ -12,15 +12,7 @@ class Create5ac8121a8ecb2ContactContactTable extends Migration
      */
     public function up()
     {
-        if(! Schema::hasTable('contact_contact')) {
-            Schema::create('contact_contact', function (Blueprint $table) {
-                $table->integer('contact_id')->unsigned()->nullable();
-                $table->foreign('contact_id', 'fk_p_135003_135003_contac_5ac8121a8ee05')->references('id')->on('contacts')->onDelete('cascade');
-                $table->integer('contact_id')->unsigned()->nullable();
-                $table->foreign('contact_id', 'fk_p_135003_135003_contac_5ac8121a8eec6')->references('id')->on('contacts')->onDelete('cascade');
-                
-            });
-        }
+
     }
 
     /**
@@ -30,6 +22,6 @@ class Create5ac8121a8ecb2ContactContactTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contact_contact');
+
     }
 }
