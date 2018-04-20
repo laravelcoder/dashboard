76053a1df4845f3a93ad7447f1254f45b80f4343
laravelcoder <pmadsen2013@gamil.comgit>
2018-04-20 15:06:27 -0600

    update

--- database/migrations/2018_03_28_204051_create_5abbd3a07cb2e_contact_contact_table.php
+++ database/migrations/2018_03_28_204051_create_5abbd3a07cb2e_contact_contact_table.php
@@ -1,35 +1,30 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class Create5abbd3a07cb2eContactContactTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        if(! Schema::hasTable('contact_contact')) {
-            Schema::create('contact_contact', function (Blueprint $table) {
-                $table->integer('contact_id')->unsigned()->nullable();
-                $table->foreign('contact_id', 'fk_p_135003_135003_contac_5abbd3a07ccd7')->references('id')->on('contacts')->onDelete('cascade');
-                $table->integer('contact_id')->unsigned()->nullable();
-                $table->foreign('contact_id', 'fk_p_135003_135003_contac_5abbd3a07cda8')->references('id')->on('contacts')->onDelete('cascade');
-                
-            });
-        }
-    }
+class Create5abbd3a07cb2eContactContactTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		if (!Schema::hasTable('contact_contact')) {
+			Schema::create('contact_contact', function (Blueprint $table) {
+				$table->integer('contact_id')->unsigned()->nullable();
+				$table->foreign('contact_id', 'fk_p_135003_135003_contac_5abbd3a07ccd7')->references('id')->on('contacts')->onDelete('cascade');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('contact_contact');
-    }
+			});
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('contact_contact');
+	}
 }
