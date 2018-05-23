e41b8e24005adab681cb8693e876a3dcf9eac475
laravelcoder <wecodelaravel@gmail.com>
2018-05-22 23:16:50 -0600

    added basic version of booking dashboard to start adding to
    basic outline of booking dashboard

---
+++ resources/views/admin/bookings_dashboards/partials/topwidgets.blade.php
@@ -0,0 +1,122 @@
+<!-- =========================================================== -->
+
+<div class="row">
+	<div class="col-lg-3 col-xs-6">
+
+		<div class="small-box bg-yellow">
+			<div class="inner">
+				<h3> </h3>
+				<h4>Today's Appointments</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-users"></i>
+			</div>
+
+		</div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-aqua">
+			<div class="inner">
+				<h3> </h3>
+				<h4>Week's Appointments</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-user-plus"></i>
+			</div>
+
+		</div>
+	</div>
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-green">
+			<div class="inner">
+				<h3> <sup style="font-size: 20px"></sup></h3>
+				<h4>Months Appointment's</h4>
+			</div>
+			<div class="icon">
+				<i class="ion ion-stats-bars"></i>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+		<div class="small-box bg-red">
+			<div class="inner">
+				<h3>being built</h3>
+				<h4>Total Searches</h4>
+			</div>
+			<div class="icon">
+				<i class="ion ion-pie-graph"></i>
+			</div>
+
+		</div>
+	</div>
+
+
+	<div class="col-md-12">
+
+
+	</div>
+
+
+</div>
+{{--
+<div class="row">
+	<div class="col-lg-3 col-xs-6">
+
+		<div class="small-box bg-green">
+			<div class="inner">
+				<h3> ?? ga:goalCompletionsAll ?? </h3>
+				<h4>Total Goal Completions</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-users"></i>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+
+		<div class="small-box bg-green">
+			<div class="inner">
+				<h3>?? ga:goal5Starts ??</h3>
+				<h4>Store Locator Submits</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-users"></i>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+
+		<div class="small-box bg-green">
+			<div class="inner">
+				<h3>?? ga:goal6Starts ??</h3>
+				<h4>Clinics-Near-you Submits</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-users"></i>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-xs-6">
+
+		<div class="small-box bg-green">
+			<div class="inner">
+				<h3> TBD </h3>
+				<h4>  </h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-users"></i>
+			</div>
+
+		</div>
+	</div>
+</div>
+--}}
+<!-- =========================================================== -->
