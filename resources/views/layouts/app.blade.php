cc24be9486275eb0124816f2a854e8522a16e45c
laravelcoder <wecodelaravel@gmail.com>
2018-05-25 01:08:28 -0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,24 @@
 
 <head>
     @include('partials.head')
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MDS48N2');</script>
+    <!-- End Google Tag Manager -->
+
+    @yield('topcss')
+    @yield('topscripts')
 </head>
 
 
 <body class="hold-transition skin-blue sidebar-mini">
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDS48N2" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 <div id="wrapper">
 
 @include('partials.topbar')
@@ -54,5 +67,8 @@
 {!! Form::close() !!}
 
 @include('partials.javascripts')
+@yield('bottomscripts')
+
+@yield('afterJQ')
 </body>
 </html>
