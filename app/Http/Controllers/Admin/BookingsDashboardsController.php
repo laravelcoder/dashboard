c08b15cc879b8ced577bef258ab88670c5939e4a
laravelcoder <wecodelaravel@gmail.com>
2018-06-01 17:27:09 -0600

    update

--- app/Http/Controllers/Admin/BookingsDashboardsController.php
+++ app/Http/Controllers/Admin/BookingsDashboardsController.php
@@ -28,6 +28,20 @@ class BookingsDashboardsController extends Controller
      */
     public function index()
     {
+
+    	$totalbookings = App\Booking::whereHas('requested_clinic')->count();
+		// $count_preferred = Booking::whereHas('requested_clinic', function($q) use ($request) {
+		//     $q->where('question_id', $request->question_id);
+		//     $q->where('pref', 1);
+		//     $q->orderBy('created_at', 'desc'); // Sort the results by latest added
+		// })->get()->count();
+
+		// foreach($count_preferred as $count)
+		// {
+		//     $count->answers->first(); // Shit should return the last first added item
+		// }
+
+
         if (! Gate::allows('booking_access')) {
             return abort(401);
         }
@@ -148,6 +162,6 @@ class BookingsDashboardsController extends Controller
             return $table->make(true);
         }
 
-        return view('admin.bookings_dashboards.index');
+        return view('admin.bookings_dashboards.index', compact('totalbookings'));
     }
 }
