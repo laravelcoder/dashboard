e8dcfa8df479e895cbde9365810eb4e4c65ca1a0
Talha Ahmed <talha5389@gmail.com>
2018-07-24 03:28:13 +0500

    Updated tracking number seeds to include country code

--- database/seeds/TrackingNumberSeed.php
+++ database/seeds/TrackingNumberSeed.php
@@ -13,14 +13,14 @@ class TrackingNumberSeed extends Seeder
     {
         $items = [
             
-            ['id' => 1, 'metrics_id' => '101030L', 'number' => '2109609827 ', 'location_id' => null, 'company_id' => 4,],
-            ['id' => 2, 'metrics_id' => '101030L', 'number' => '2109609947', 'location_id' => null, 'company_id' => 4,],
-            ['id' => 3, 'metrics_id' => '101030L', 'number' => '2109609977', 'location_id' => null, 'company_id' => 4,],
-            ['id' => 4, 'metrics_id' => '198010L', 'number' => '8087400055', 'location_id' => 3, 'company_id' => 2,],
-            ['id' => 5, 'metrics_id' => '198010L', 'number' => '8087400051', 'location_id' => 3, 'company_id' => 2,],
-            ['id' => 6, 'metrics_id' => '198010L', 'number' => '8087400054', 'location_id' => 3, 'company_id' => 2,],
-            ['id' => 7, 'metrics_id' => '198010L', 'number' => '8087400052', 'location_id' => 3, 'company_id' => 2,],
-            ['id' => 8, 'metrics_id' => '198010L', 'number' => '8086701008', 'location_id' => null, 'company_id' => 2,],
+            ['id' => 1, 'metrics_id' => '101030L', 'number' => '+12109609827 ', 'location_id' => null, 'company_id' => 4,],
+            ['id' => 2, 'metrics_id' => '101030L', 'number' => '+12109609947', 'location_id' => null, 'company_id' => 4,],
+            ['id' => 3, 'metrics_id' => '101030L', 'number' => '+12109609977', 'location_id' => null, 'company_id' => 4,],
+            ['id' => 4, 'metrics_id' => '198010L', 'number' => '+18087400055', 'location_id' => 3, 'company_id' => 2,],
+            ['id' => 5, 'metrics_id' => '198010L', 'number' => '+18087400051', 'location_id' => 3, 'company_id' => 2,],
+            ['id' => 6, 'metrics_id' => '198010L', 'number' => '+18087400054', 'location_id' => 3, 'company_id' => 2,],
+            ['id' => 7, 'metrics_id' => '198010L', 'number' => '+18087400052', 'location_id' => 3, 'company_id' => 2,],
+            ['id' => 8, 'metrics_id' => '198010L', 'number' => '+18086701008', 'location_id' => null, 'company_id' => 2,],
 
         ];
 
