c1bfae3f6449989fc40c05b12b86a434e7cd36a6
WeCodeLaravel <wecodelaravel@gmail.com>
2019-11-06 13:15:17 -0700

    Update call-tracking-api.php

--- resources/views/admin/call_metrics/partials/call-tracking-api.php
+++ resources/views/admin/call_metrics/partials/call-tracking-api.php
@@ -1,7 +1,6 @@
 <?php 
-
-
-curl --request GET --url 'https://api.calltrackingmetrics.com/api/v1/accounts' --header 'Authorization: {{Basic Auth}}'   --header 'Content-Type: application/json'
-
-
- ?>
+	curl --request GET \
+	  --url 'https://api.calltrackingmetrics.com/api/v1/accounts' \
+	  --header 'Authorization: {{Basic Auth}}' \
+	  --header 'Content-Type: application/json' 
+?>
